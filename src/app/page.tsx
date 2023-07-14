831f9255539bb79a11e1bef6c226b7268fb7fef0
김지호 <crsn1111@gmail.com>
2023-07-15 00:37:02 +0900

    feat: toss use-funnel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Home() {
-  return <></>;
-}
+const Home = () => {
+  return <Link href="/funnel">가입 페이지로 이동</Link>;
+};
+
+export default Home;
