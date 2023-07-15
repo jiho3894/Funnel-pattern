b98d1c64d0db719542ca8b3ff7c9140f0daf8c84
김지호 <crsn1111@gmail.com>
2023-07-15 15:20:48 +0900

    feat: build page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,24 +1,24 @@
 'use client';
 
-import { useFunnel } from '@toss/use-funnel';
+// import { useFunnel } from '@toss/use-funnel';
 
-const Signup = () => {
-  const [Funnel, setStep] = useFunnel(['test', '전화번호', '가입완료'] as const, {
-    initialStep: 'test',
-  });
-  return (
-    <Funnel>
-      <Funnel.Step name="test">
-        <div>Email</div>
-      </Funnel.Step>
-      <Funnel.Step name="전화번호">
-        <div>Email</div>
-      </Funnel.Step>
-      <Funnel.Step name="가입완료">
-        <div>Email</div>
-      </Funnel.Step>
-    </Funnel>
-  );
-};
+// const Signup = () => {
+//   const [Funnel, setStep] = useFunnel(['test', '전화번호', '가입완료'] as const, {
+//     initialStep: 'test',
+//   });
+//   return (
+//     <Funnel>
+//       <Funnel.Step name="test">
+//         <div>Email</div>
+//       </Funnel.Step>
+//       <Funnel.Step name="전화번호">
+//         <div>Email</div>
+//       </Funnel.Step>
+//       <Funnel.Step name="가입완료">
+//         <div>Email</div>
+//       </Funnel.Step>
+//     </Funnel>
+//   );
+// };
 
-export default Signup;
+// export default Signup;
